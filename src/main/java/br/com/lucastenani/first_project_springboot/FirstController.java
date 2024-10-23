8a9528b81844af1872116557ddc3338b1a94c866
lucastenani <lucas.tenani3@gmail.com>
2024-10-23 18:14:19 -0300

    feat - Create first controller for the application

---
+++ src/main/java/br/com/lucastenani/first_project_springboot/FirstController.java
@@ -0,0 +1,14 @@
+package br.com.lucastenani.first_project_springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class FirstController {
+    @GetMapping("/world")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
