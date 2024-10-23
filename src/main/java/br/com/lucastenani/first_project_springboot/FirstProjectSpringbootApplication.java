ac5f7cf351bb5b3ccce90aafb95cf23c050f81c5
lucastenani <lucas.tenani3@gmail.com>
2024-10-23 16:45:33 -0300

    first commit

---
+++ src/main/java/br/com/lucastenani/first_project_springboot/FirstProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucastenani.first_project_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucastenani/first_project_springboot/FirstProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucastenani.first_project_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
